e4ebbe577baea739e9ccf83d80a1748897149897
rixong <rixong@gmail.com>
2020-04-03 10:16:27 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,41 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {};
+  constructor() {
+    super()
+    this.state = {
+      username: undefined,
+      password: undefined
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.username && this.state.password) {
+      this.props.handleLogin(this.state);
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      input: ''
+    };
   }
+
+handleChange = (event) => {
+  // console.log(event.target.value);
+  this.setState({
+    input: event.target.value,
+  })
+}
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.input}/>
+        <br></br>
+    <label>{this.props.maxChars - this.state.input.length} characters remaining</label>
       </div>
     );
   }
